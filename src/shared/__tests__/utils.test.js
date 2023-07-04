6ccef0cf68ff42d634209f80f03ad369abaf6007
Blake Taylor <sephnescence@gmail.com>
2023-07-04 11:39:37 +1000

    Finished 'Compile Modules with Babel in Jest Tests'

---
+++ src/shared/__tests__/utils.test.js
@@ -0,0 +1,13 @@
+/*
+See .babelrc.js for how we can compile modules in testing without
+relying on webpack
+
+If we weren't to do that, we'd get this error instead
+> SyntaxError: Cannot use import statement outside a module
+*/
+
+import { getFormattedValue } from '../utils'
+
+test('formats the value', () => {
+    expect(getFormattedValue('1234.0')).toBe('1,234.0')
+})
