b9fd11014a8e6d66cfc3731bee41d74bcb97c99e
Blake Taylor <sephnescence@gmail.com>
2023-07-12 18:23:23 +1000

    Finished 'Test an Emotion Styled UI with Custom Jest Snapshot Serializers' and 'Support Custom Module Resolution with Jest moduleDirectories'

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
+// import path from 'path' // The ide would love to do this, but imports don't work outside of webpack, so you must call require
+const path = require('path')
+
 module.exports = {
   testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: ['node_modules', path.join(__dirname, 'src'), 'shared'],
   moduleNameMapper: {
     '\\.module\\.css': 'identity-obj-proxy',
     '\\.css$': require.resolve('./test/style-mock.js'),
   },
+  snapshotSerializers: ['@emotion/jest/serializer'],
 }
--- src/shared/__tests__/calculator-display.test.js
+++ src/shared/__tests__/calculator-display.test.js
@@ -5,15 +5,26 @@ import CalculatorDisplay from '../calculator-display'
 test('renders', () => {
   const {container} = render(<CalculatorDisplay value="0.2" />)
   expect(container.firstChild).toMatchInlineSnapshot(`
+    .emotion-0 {
+      position: relative;
+      color: white;
+      background: #1c191c;
+      line-height: 130px;
+      font-size: 6em;
+      -webkit-flex: 1;
+      -ms-flex: 1;
+      flex: 1;
+    }
+
     <div
-      class="css-lq9ahq-calculator-display--CalculatorDisplay"
+      class="emotion-0"
     >
       <div
         class="autoScalingText"
         data-testid="total"
         style="transform: scale(1,1);"
       >
-        0
+        0.2
       </div>
     </div>
   `)
